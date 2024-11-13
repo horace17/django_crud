5eb3157327e8ea69481290c82b01440c81b194e1
horace <hocedrix2010@gmail.com>
2024-11-13 13:41:56 +0300

    received user input and stored

--- students/urls.py
+++ students/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('insert/', views.insert, name='insert'),
+    path('insert_data/', views.insert_data, name='insert_data'),
+    path('view_data/', views.view_data, name='view_data'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from students.models import Student
 
 # Create your views here.
 def index(request):
@@ -6,4 +7,23 @@ def index(request):
 
 def insert(request):
     return render(request, 'insert.html')
+
+
+def insert_data(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        age = request.POST['age']
+        phone = request.POST['phone']
+        image = request.FILES['image']
+
+        student = Student(name=name, email=email, age=age, phone=phone, image=image)
+        student.save()
+        return redirect('/')
+
+    return render(request, 'insert.html')
+
+def view_data(request):
+    student = Student.objects.all()
+    return render(request, 'viewdata.html', {"students": student})
 
